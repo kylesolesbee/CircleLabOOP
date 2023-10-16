8eaef6457caeaadb4623090d1b47629ac4b9ad95
Kyle Solesbee <ksolesbee99@gmail.com>
2023-10-16 16:57:57 -0400

    CIRCLE OOP DONE :D

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CircleLabOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            double radius = Validator.GetValidRadius();
+
+            Circle circle = new Circle(radius);
+
+            DisplayCircleInfo(circle);
+
+            while (Validator.ShouldCircleGrow())
+            {
+                circle.Grow();
+                Console.WriteLine("The circle is magically growing.");
+                DisplayCircleInfo(circle);
+            }
+
+            Console.WriteLine($"Goodbye. The circle’s final radius is {circle.GetRadius()}.");
+        }
+
+        static void DisplayCircleInfo(Circle circle)
+        {
+            Console.WriteLine("Diameter: " + circle.CalculateDiameter());
+            Console.WriteLine("Circumference: " + circle.CalculateCircumference().ToString("F3"));
+            Console.WriteLine("Area: " + circle.CalculateArea().ToString("F4"));
+        }
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleLabOOP
+{
+    internal class Validator
+    {
+        public static double GetValidRadius()
+        {
+            double radius;
+            do
+            {
+                Console.Write("Enter radius: ");
+            } while (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0);
+            return radius;
+        }
+
+        public static bool ShouldCircleGrow()
+        {
+            bool isValidResponse = false;
+            bool shouldGrow = false;
+
+            do
+            {
+                Console.Write("Should the circle grow? (y/n): ");
+                string response = Console.ReadLine().Trim().ToLower();
+
+                try
+                {
+                    shouldGrow = GetYesNoResponse(response);
+                    isValidResponse = true;
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Invalid input. Please enter 'y' or 'n'.");
+                }
+            } while (!isValidResponse);
+
+            return shouldGrow;
+        }
+
+        private static bool GetYesNoResponse(string response)
+        {
+            if ("yes".Contains(response.Trim().ToLower()))
+            {
+                return true;
+            }
+            else if ("no".Contains(response.Trim().ToLower()))
+            {
+                return false;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid response");
+            }
+        }
+    }
+}
